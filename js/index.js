a771811f830cdf267bd8274d5931c9d914728159
mkyang02 <maneekyang@gmail.com>
2022-08-03 16:08:53 -0500

    attempted javascript. was not the greatest but I tried

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".card").hover(function(){
+      $(this).css("background-color", "#fc434a");
+      }, function(){
+      $(this).css("background-color", "white");
+    });
+  });
